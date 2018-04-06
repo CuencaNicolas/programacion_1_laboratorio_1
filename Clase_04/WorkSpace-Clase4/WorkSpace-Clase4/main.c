2d3cf483875788bdc04385a0f1495eac32d98565
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-04-06 10:20:08 -0300

    Add files via upload

---
+++ Clase_04/WorkSpace-Clase4/WorkSpace-Clase4/main.c
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+#include "Funciones.h"
+
+
+
+
+int main()
+{
+    int edad;
+    int legajo;
+
+    edad = pedirEntero("Ingrese edad: ", 18,60);
+    legajo = pedirEntero("Ingrese legajo: ",1,2000);
+
+    return 0;
+}
+
+
---
+++ Clase_04/WorkSpace-Clase4/sumas/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Funciones.h"
+
+/*prototipo 1*/
+void sumar1(void);
+/*prototipo 2*/
+void sumar2(int numeroUno,int numeroDos);
+/*prototipo 3*/
+int sumar3();
+/*prototipo 4*/
+int sumar4(int,int);
+
+int main()
+{
+    //sumar1();
+
+    //sumar2(4,9);
+
+    int resultado;
+    //resultado= sumar3();
+    //printf("La suma es %d",suma)
+    int numeroUno;
+    int numeroDos;
+
+    numeroUno = pedirEntero("Ingrese un numero ",1,1000);
+    numeroDos = pedirEntero("Ingrese un numero",1,1000);
+
+    resultado = sumar4(numeroUno,numeroDos);
+
+    printf("El resultado es: %d",resultado);
+
+    return 0;
+}
+
+/*prototipo 1*/
+
+void sumar1(void)
+
+{
+    int numeroUno;
+    int numeroDos;
+    int suma;
+
+    numeroUno = pedirEntero("Ingrese un numero ",1,1000);
+    numeroDos = pedirEntero("Ingrese un numero",1,1000);
+
+    suma = numeroUno + numeroDos;
+
+    printf("La suma es: %d", suma);
+
+}
+
+/*prototipo 2*/
+
+void sumar2(int numeroUno,int numeroDos)
+{
+    int suma;
+
+    suma = numeroUno + numeroDos;
+
+    printf("La suma es: %d",suma);
+
+}
+
+/*prototipo 3*/
+
+int sumar3()
+{
+
+    int numeroUno;
+    int numeroDos;
+    int suma;
+
+    numeroUno = pedirEntero("Ingrese un numero ",1,1000);
+    numeroDos = pedirEntero("Ingrese un numero",1,1000)
+
+    suma = numeroUno + numeroDos;
+
+    return suma;
+
+}
+
+/*prorotipo 4*/
+
+int sumar4(int numeroUno,int numeroDos)
+
+{
+    int suma;
+
+    suma = numeroUno + numeroDos;
+
+
+}
